3dab7c6d5636e4160870f06401f53e6e080157dd
Dante4rt <rxmxdhxni@gmail.com>
2024-09-01 17:19:14 +0700

    fix: error in 'c' var

--- index.js
+++ index.js
@@ -1,98 +1,83 @@
-const s = c;
-(function (t, e) {
-  const n = c,
-    o = t();
-  while (!![]) {
-    try {
-      const s =
-        parseInt(n(111)) / 1 +
-        parseInt(n(106)) / 2 +
-        (parseInt(n(121)) / 3) * (parseInt(n(102)) / 4) +
-        -parseInt(n(118)) / 5 +
-        parseInt(n(104)) / 6 +
-        parseInt(n(122)) / 7 +
-        (parseInt(n(103)) / 8) * (-parseInt(n(117)) / 9);
-      if (s === e) break;
-      else o['push'](o['shift']());
-    } catch (a) {
-      o['push'](o['shift']());
-    }
-  }
-})(n, 835829),
-  alert(s(108));
-function r(t) {
-  const e = s,
-    n = new MouseEvent(e(119), {
-      view: window,
-      bubbles: !![],
-      cancelable: !![],
-    });
-  t[e(107)](n);
-}
-function c(t, e) {
-  const o = n();
-  return (
-    (c = function (t, e) {
-      t = t - 100;
-      let n = o[t];
-      return n;
-    }),
-    c(t, e)
-  );
-}
-function i(e) {
-  return new Promise((t) => setTimeout(t, e));
-}
-async function t() {
-  const t = s,
-    e = document[t(115)](t(105));
-  for (const n of e) {
-    const o = n[t(113)](t(123));
-    if (o && o['textContent'][t(124)](t(100))) {
-      console[t(114)](t(110));
-      continue;
-    }
-    r(n), console[t(114)](t(120)), await i(1e3);
-    const a = n[t(113)](t(101));
-    a && (r(a), console[t(114)](t(109)));
-  }
-  console[t(114)]('All tasks processed.'),
-    console[t(114)]('Subscribe: https://t.me/HappyCuanAirdrop');
-}
-function n() {
-  const t = [
-    'Completed',
-    'button[data-verify-button-status="idle"][data-verify-button-disabled="false"]',
-    '5408HeiGNz',
-    '40HihmFa',
-    '34188gvVhtr',
-    'div._accordian_base_container_1vd77_1',
-    '992688QRDiNH',
-    'dispatchEvent',
-    'Intract Auto Complete Tasks by HappyCuanAirdrop \nSubscribe: https://t.me/HappyCuanAirdrop',
-    'Clicked verify button',
-    'Skipping completed task',
-    '1055079GnpekC',
-    'then',
-    'querySelector',
-    'log',
-    'querySelectorAll',
-    'All tasks processed! ^_^\nSubscribe: https://t.me/HappyCuanAirdrop',
-    '1897731SatwTq',
-    '4769845pXyIZI',
+const _0x5bf093 = _0x59e3;
+function _0x2835() {
+  const e = [
     'click',
+    'Skipping completed task',
+    '6749407KHrLTT',
+    'then',
+    '83475UebPmo',
+    '620514DQSgkx',
+    'All tasks processed! ^_^\nSubscribe: https://t.me/HappyCuanAirdrop',
     'Clicked task container',
-    '1644tieEKB',
-    '3822532atZcYv',
+    '17300072OJLxxB',
+    '1782294mHbzbk',
+    'querySelectorAll',
+    'log',
+    '1560750MgpqPU',
+    '1783716hMsgtJ',
+    'Clicked verify button',
     'div[data-badge-size="md"][data-button-badge-variant="tertiary"]',
-    'includes',
+    'button[data-verify-button-status="idle"][data-verify-button-disabled="false"]',
+    'Intract Auto Complete Tasks by HappyCuanAirdrop \nSubscribe: https://t.me/HappyCuanAirdrop',
+    'All tasks processed.',
+    '3kKhEfp',
+    'dispatchEvent',
+    'textContent',
   ];
-  n = function () {
-    return t;
-  };
-  return n();
+  return (_0x2835 = function () {
+    return e;
+  })();
 }
-t()[s(112)](() => {
-  const t = s;
-  alert(t(116));
-});
+function clickElement(e) {
+  var t = _0x5bf093,
+    n = new MouseEvent(t(434), { view: window, bubbles: !0, cancelable: !0 });
+  e[t(432)](n);
+}
+function delay(t) {
+  return new Promise((e) => setTimeout(e, t));
+}
+async function processTasks() {
+  var e = _0x5bf093;
+  for (const n of document[e(444)]('div._accordian_base_container_1vd77_1')) {
+    var t = n.querySelector(e(427));
+    t && t[e(433)].includes('Completed')
+      ? console[e(445)](e(435))
+      : (clickElement(n),
+        console[e(445)](e(441)),
+        await delay(1e3),
+        (t = n.querySelector(e(428))) &&
+          (clickElement(t), console[e(445)](e(426))));
+  }
+  console[e(445)](e(430)),
+    console[e(445)]('Subscribe: https://t.me/HappyCuanAirdrop');
+}
+function _0x59e3(e, t) {
+  const n = _0x2835();
+  return (_0x59e3 = function (e, t) {
+    return (e -= 426), n[e];
+  })(e, t);
+}
+(function () {
+  for (var e = _0x59e3, t = _0x2835(); ; )
+    try {
+      if (
+        536912 ==
+        +parseInt(e(438)) +
+          (parseInt(e(439)) / 2) * (parseInt(e(431)) / 3) +
+          -parseInt(e(447)) / 4 +
+          -parseInt(e(446)) / 5 +
+          -parseInt(e(443)) / 6 +
+          -parseInt(e(436)) / 7 +
+          parseInt(e(442)) / 8
+      )
+        break;
+      t.push(t.shift());
+    } catch (e) {
+      t.push(t.shift());
+    }
+})(),
+  alert(_0x5bf093(429)),
+  processTasks()[_0x5bf093(437)](() => {
+    var e = _0x5bf093;
+    alert(e(440));
+  });
